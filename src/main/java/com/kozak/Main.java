81282cc6077b14cfb0fcb1d4b60424a5561814cc
Ivan Kozak <ivasykkozak611@gmail.com>
2023-01-23 17:07:25 +0200

    add cicle and Scanner

--- src/main/java/com/kozak/Main.java
+++ src/main/java/com/kozak/Main.java
@@ -1,7 +1,14 @@
 package com.kozak;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        Scanner in = new Scanner(System.in);
+        int k = in.nextInt();
+        for (int i = 0; i < k; i++) {
+            System.out.println("Hello world!!! = " + k);
+        }
+
     }
 }
